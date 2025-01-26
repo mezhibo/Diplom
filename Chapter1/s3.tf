05c30a88e619d2eb6e01ec2f14b3874935026e43
mezhibo <155834085+mezhibo@users.noreply.github.com>
2025-01-26 19:00:12 +0300

    Create s3.tf

---
+++ Chapter1/s3.tf
@@ -0,0 +1,11 @@
+# Создадим бакет с использованием ключа
+resource "yandex_storage_bucket" "state_storage" {
+  bucket     = local.bucket_name
+  access_key = yandex_iam_service_account_static_access_key.terraform_service_account_key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.terraform_service_account_key.secret_key
+
+  anonymous_access_flags {
+    read = false
+    list = false
+  }
+}
